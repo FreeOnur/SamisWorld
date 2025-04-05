90d60cd7cc6b8605c71f80faa67a2d09f9e0e009
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-04-05 13:00:27 +0200

    Added Damage Funktion

---
+++ Samis World/Assets/Scripts/Player/Skills/AlleSkills/SkillStrength.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillStrength : Skill
+{
+    public SkillStrength() : base("Strength", 1, 100)
+    {
+
+    }
+}
