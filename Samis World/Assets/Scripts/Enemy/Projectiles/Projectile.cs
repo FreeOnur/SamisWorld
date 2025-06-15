25d6b2e93a4c5dc5b79afe565b95a3046bc66a84
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-06-15 02:18:13 +0200

    Added new Skill Projectile to my Player and made the projectiles compatible

--- Samis World/Assets/Scripts/Enemy/Projectiles/Projectile.cs
+++ Samis World/Assets/Scripts/Enemy/Projectiles/Projectile.cs
@@ -4,6 +4,7 @@ public class Projectile : MonoBehaviour
 {
     public float damage = 1f;
     public float lifetime = 5f;
+    public bool isEnemyProjectile = false;
 
     void Start()
     {
@@ -12,7 +13,18 @@ public class Projectile : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (!isEnemyProjectile && other.CompareTag("Enemy"))
+        {
+            IDamagable target = other.GetComponent<IDamagable>();
+            if (target != null)
+            {
+                target.Damage(damage);
+            }
+
+            Destroy(gameObject);
+        }
+        // Wenn das Projektil vom Gegner ist, soll es nur den Spieler treffen
+        else if (isEnemyProjectile && other.CompareTag("Player"))
         {
             IDamagable target = other.GetComponent<IDamagable>();
             if (target != null)
--- Samis World/Assets/Scripts/Player/Skills/PlayerAttack.cs
+++ Samis World/Assets/Scripts/Player/Skills/PlayerAttack.cs
@@ -1,34 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class PlayerAttack : MonoBehaviour
 {
     [Header("Skill Related Bools For Activation")]
-    public bool fireballUnlocked = false;
+    public bool fireballUnlocked = true;
     [Header("Projectile Attack")]
     public GameObject projectilePrefab;
     public Transform firePoint;
     public float projectileSpeed = 5f;
     private Transform target;
+    private EnemyHealth enemyHealth;
+
     void Start()
     {
         target = GetNearestEnemy();
     }
     public void PerformFireballAttack()
     {
+        if (target == null)
+        {
+            target = GetNearestEnemy();
+        }
+
         if (!fireballUnlocked || projectilePrefab == null || firePoint == null || target == null)
             return;
 
         Vector2 direction = (target.position - firePoint.position).normalized;
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        projectile.GetComponent<Projectile>().isEnemyProjectile = false;
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
             rb.velocity = direction * projectileSpeed;
         }
     }
+
 
 
     private Transform GetNearestEnemy()
@@ -52,6 +62,9 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            PerformFireballAttack();
+        }
     }
 }
